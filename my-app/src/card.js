569f41a16f5fb53fb520f2d52f05dfa854d5746f
Avishi2511 <118925593+Avishi2511@users.noreply.github.com>
2023-08-05 18:46:31 +0530

    index

---
+++ my-app/src/card.js
@@ -0,0 +1,21 @@
+const card=[
+  {
+      id:1,
+      title:"Stocks",
+      meaning:" ",
+  },
+
+  {
+      id:2,
+      title:"Mutual Funds",
+      meaning: " ",
+  },
+
+  {
+      id:3,
+      title:"Real Estate",
+      meaning: " "
+  },
+];
+
+export default card;
